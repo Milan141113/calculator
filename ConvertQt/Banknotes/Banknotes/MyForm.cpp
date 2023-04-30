1323b89cad859af09f42ceaca8ab3bb115741cd3
WensdeyAdams <126066327+WensdeyAdams@users.noreply.github.com>
2023-04-30 14:22:54 +0300

    Update MyForm.cpp

--- ConvertQt/Banknotes/Banknotes/MyForm.cpp
+++ ConvertQt/Banknotes/Banknotes/MyForm.cpp
@@ -1,10 +1,12 @@
 #include "MyForm.h"
+#include "locale"
 
 using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThreadAttribute]
 int main(array<String^>^ args) {
+	setlocale( LC_ALL, "Russian");
 	Application::SetCompatibleTextRenderingDefault(false);
 	Application::EnableVisualStyles();
 	Banknotes::MyForm form;
